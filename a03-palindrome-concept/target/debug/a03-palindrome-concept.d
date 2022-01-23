80c0066ed13d8e084cccdf23eed0650aba5d26fb
Phisinee Sagulvanitchaporn <phisinee.s@pomelofashion.com>
2022-01-23 09:47:34 +0700

    fix: add gitignore

--- a03-palindrome-concept/target/debug/a03-palindrome-concept.d
+++
@@ -1,1 +0,0 @@
-/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/target/debug/a03-palindrome-concept: /Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/src/main.rs
--- a03-palindrome-concept/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.d
+++ a03-palindrome-concept/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.d
@@ -1,5 +1,5 @@
-/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.rmeta: src/main.rs
+/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept-fix/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.rmeta: src/main.rs
 
-/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.d: src/main.rs
+/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept-fix/target/rls/debug/deps/a03_palindrome_concept-34fe0d14fd53e800.d: src/main.rs
 
 src/main.rs:
