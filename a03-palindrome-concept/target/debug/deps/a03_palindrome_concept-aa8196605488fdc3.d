b607d35bd3f5f467d3c635a89b442ef053b555ba
Phisinee Sagulvanitchaporn <phisinee.s@pomelofashion.com>
2022-01-23 09:44:16 +0700

    fix: add gitignore

--- a03-palindrome-concept/target/debug/deps/a03_palindrome_concept-aa8196605488fdc3.d
+++
@@ -1,5 +0,0 @@
-/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/target/debug/deps/a03_palindrome_concept-aa8196605488fdc3.rmeta: src/main.rs
-
-/Users/beaubaby/Documents/Code/Rust/KubeOps-ACA/a03-palindrome-concept/target/debug/deps/a03_palindrome_concept-aa8196605488fdc3.d: src/main.rs
-
-src/main.rs:
