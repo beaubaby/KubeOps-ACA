c5a0b9b22cfd2d065ab1fb9e6737e568cca937f6
Phisinee Sagulvanitchaporn <phisinee.s@pomelofashion.com>
2022-01-23 09:50:35 +0700

    fix: add gitignore

---
+++ a03-palindrome-concept-fix/README.md
@@ -0,0 +1,26 @@
+# Palindrome in Rust and break with checking input the word rapidly until 'END' program 
+
+This is the part of Palindrome homework 2 to practice in KubeOps Academy #1
+
+### The structure of source code (Copied from [a02-palindrome-concept](../a02-palindrome-concept)
+
+* Before the code writing, can use this command below to input the structure of code
+
+`cargo new <folder name of source code>`
+
+```
+./
+├── src
+│   └── main.rs
+├── .gitignore
+├── cargo.toml
+├── README.MD
+```
+
+* After finish the code writing, should be run the test
+
+`cargo test`
+
+* Execute the source code and logic
+
+`cargo run`
---
+++ a03-palindrome-concept-fix/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use regex::Regex;
+
+fn main() {
+    loop {
+    let mut word = String::new();
+    println!("Please enter the word to check is it Palindrome or not : ");
+    io::stdin().read_line(&mut word).expect("The word is incorrect, Please input again");
+    if word.trim().to_uppercase() == "END" { break; }
+    check_palindrome(word);
+    }
+    println!("End Program");
+}
+
+fn check_palindrome(word: String) {
+    // set all alphanumeric to Lowercase to prepare pre-word //
+    let pre_word: String = word.trim().to_lowercase();
+    let palindrome_word: &str = pre_word.as_str();
+
+    // remove non-alphanumeric from the word
+    let regex = Regex::new(r"[^a-zA-Z0-9ก-ฮ]").unwrap();
+    let palindrome_word = &regex.replace_all(palindrome_word, "");
+
+    let mut palindrome: bool = true;
+
+    for (a1, a2) in palindrome_word.chars().enumerate() {
+    if a1 == palindrome_word.len() / 2 {
+        break;
+        }
+    if a2 != palindrome_word.chars().rev().nth(a1).unwrap() {
+        palindrome = false;
+        break;
+        }
+    }
+
+    if palindrome {
+        println!("\"{}\" is a palindrome word", word.trim());
+    } else {
+        println!("\"{}\" is not a palindrome word", word.trim());
+    }
+}
